fccba2f0571ab8b35cb0b9e8e5998436c6897549
gjgjsgs <3872-8u3-gin0@ezweb.ne.jp>
2019-11-10 20:12:23 +0900

    GoForwardUnityChan  課題　初めてのコミット

---
+++ Assets/BlockSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSound : MonoBehaviour {
+
+    
+
+    public AudioClip sound;
+
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Cube")
+        {
+            audioSource.PlayOneShot(sound);
+
+        }
+
+
+    }
+}
